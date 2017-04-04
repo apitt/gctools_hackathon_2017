f29f9863aa39fad62870b4123228c64aeabf01d3
Giassona <Alexandra.Giasson@pwgsc-tpsgc.gc.ca>
2017-04-04 18:21:00 -0400

    Add files via upload

---
+++ docker_scripts/0_install_gcpedia.sh
@@ -0,0 +1,23 @@
+#clone GCPedia code if not already done:
+if [ ! -d "gcpedia" ]; then
+    echo '*** Getting GcPedia code ***'
+    git clone https://github.com/gctools-outilsgc/gcpedia.git
+fi
+
+cd gcpedia
+
+#install Wikimedia dependencies if not already done:
+if [ ! -d "vendor" ]; then
+    echo '*** Getting Vendor code ***'
+    git clone https://gerrit.wikimedia.org/r/p/mediawiki/vendor.git 
+fi
+
+#TODO: I should verify why this file is causing some variables to be duplicated in the php site.
+#But, for now, we will just delete its content.
+cd vendor/wikimedia/timestamp/src
+echo '<?php' > defines.php
+
+#Create a directory for Mariadb service
+mkdir data
+
+
